6301ec6fe54d784a123fd4345d1666b9a2c7e1be
Mustafa Dalga <mustafadalgaa@gmail.com>
2021-07-13 11:05:19 +0300

    cleanup

--- service-worker.js
+++ service-worker.js
@@ -1,98 +1,43 @@
-// var CACHE_NAME = 'my-site-cache-v4';
-// var urlsToCache = [
-//     // '/',
-//     // '/assets/css/main.css',
-//     // '/assets/img/book.png',
-//     // '/assets/js/main.js',
-//     '/assets/icons/1.svg'
-// ];
-
-
-// //Install a service worker
-
-// self.addEventListener('install', function(event) {
-//     // Perform install steps
-//     event.waitUntil(
-//         caches.open(CACHE_NAME)
-//         .then(function(cache) {
-//             console.log('Opened cache..');
-//             return cache.addAll(urlsToCache);
-//         })
-//     );
-// });
-
-// // Cache and return requests
-
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//         caches.match(event.request)
-//         .then(function(response) {
-//             // Cache hit - return response
-//             if (response) {
-//                 return response;
-//             }
-
-//             return fetch(event.request).then(response => {
-
-//                 // Check if we received a valid response
-//                 if (!response || response.status !== 200 || response.type !== 'basic' || response) {
-//                     return response;
-//                 }
-
-//                 var responseToCache = response.clone();
-
-//                 caches.open(CACHE_NAME)
-//                     .then(cache => {
-//                         cache.put(event.request, responseToCache);
-//                     });
-
-//                 return response;
-
-//             });
-//         })
-//     );
-// });
-
-// self.addEventListener('activate', function(event) {
-
-//     var cacheAllowlist = ['my-site-cache-v4'];
-
-//     event.waitUntil(
-//         caches.keys().then(function(cacheNames) {
-//             return Promise.all(
-//                 cacheNames.map(function(cacheName) {
-//                     console.log(cacheName, cacheAllowlist.indexOf(cacheName) === -1)
-//                     if (cacheAllowlist.indexOf(cacheName) === -1) {
-//                         return caches.delete(cacheName);
-//                     }
-//                 })
-//             );
-//         })
-//     );
-// });
-
+const expectedCaches = ['static-v2'];
 
 self.addEventListener('install', event => {
-    console.log('V1 installing…');
+    console.log('V2 installing…');
+    self.skipWaiting(); //Skip the waiting phase
 
-    // cache a cat SVG
+
+    // cache a horse SVG into a new cache, static-v2
     event.waitUntil(
-        caches.open('static-v1').then(cache => cache.add('/assets/img/people.png'))
+        caches.open(expectedCaches[0]).then(cache => cache.add('/assets/img/animal.png'))
     );
 });
 
 self.addEventListener('activate', event => {
-    console.log('V1 now ready to handle fetches!');
+    // delete any caches that aren't in expectedCaches
+    // which will get rid of static-v1
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                console.log(key)
+                if (!expectedCaches.includes(key)) {
+                    return caches.delete(key);
+                }
+            })
+        )).then(() => {
+            console.log('V2 now ready to handle fetches!');
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
     const url = new URL(event.request.url);
 
-    // serve the cat SVG from the cache if the request is
+    // serve the horse SVG from the cache if the request is
     // same-origin and the path is '/dog.svg'
+
     console.log(url)
-    if (url.origin == location.origin && url.pathname == '/assets/img/animal.png') {
-        event.respondWith(caches.match('/assets/img/people.png'));
+    if (url.origin == location.origin && url.pathname == '/assets/img/people.png') {
+        event.respondWith(caches.match('/assets/img/animal.png'));
     }
 });
+
 self.caches.c
