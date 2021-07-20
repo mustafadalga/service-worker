a3330a4001f50f94530f8c9b67906c6a64a6e10c
Mustafa Dalga <mustafadalgaa@gmail.com>
2021-07-21 00:18:19 +0300

    cleanup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 console.log("main js loaded")
 document.body.style.background = "yellow";
-// document.body.style.background = "red";
+document.body.style.background = "red";
 // document.body.style.background = "black";
 // document.body.style.background = "blue";
 
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheVersion = 'v1.0.7';
+const cacheVersion = 'v1.0.8';
 
 self.addEventListener('install', event => {
     console.log('installing…');
